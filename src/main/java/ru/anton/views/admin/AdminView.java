d46776766084c19a36f46168ccf716342fa86f7b
Anton Zhiliakov <gofranich@gmail.com>
2021-03-07 22:18:08 +0600

    Add AdminView, UserEditor.

--- src/main/java/ru/anton/views/admin/AdminView.java
+++ src/main/java/ru/anton/views/admin/AdminView.java
@@ -1,19 +1,46 @@
 package ru.anton.views.admin;
 
-import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.PageTitle;
-import ru.anton.views.main.MainView;
+import ru.anton.data.entity.User;
+import ru.anton.data.repository.UserRepository;
 
 @CssImport("./views/admin/admin-view.css")
 @PageTitle("Admin")
 public class AdminView extends Div {
 
-    public AdminView() {
-        addClassName("admin-view");
-        add(new Text("Content placeholder"));
+    private final UserRepository userRepository;
+
+    private final UserEditor editor;
+
+    final Grid<User> grid;
+
+
+    public AdminView(UserRepository userRepository, UserEditor editor) {
+        this.userRepository = userRepository;
+        this.editor = editor;
+
+        this.grid = new Grid<>(User.class);
+        add(grid, editor);
+        grid.setColumns("id", "username", "role");
+        grid.getColumnByKey("id").setWidth("50px").setFlexGrow(0);
+        grid.asSingleSelect().addValueChangeListener(e->{
+            editor.editUser(e.getValue());
+        });
+        editor.setChangeHandler(() ->{
+            editor.setVisible(false);
+
+        });
+        listUsers();
     }
 
+    private void listUsers() {
+        grid.setItems(userRepository.findAll());
+    }
+
+    public UserEditor getEditor() {
+        return editor;
+    }
 }
